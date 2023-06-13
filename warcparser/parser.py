9c62397dbe0016e08ec1534170c2bac450bda43e
Taras Khrystenko <105317386+Tapasje@users.noreply.github.com>
2023-06-13 14:35:55 +0200

    Parse all valid .html w/ <!DOCTYPE

--- warcparser/parser.py
+++ warcparser/parser.py
@@ -5,55 +5,158 @@ import re
 import logging
 from tqdm import tqdm
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+from langdetect import detect_langs
+
+def sanitize_dirname(dirname):
+    # Remove invalid characters
+    dirname = re.sub(r'[<>:"/\\|?*]', '', dirname)
+
+    # Remove leading/trailing whitespace and dots
+    dirname = dirname.strip(' .')
+
+    # Replace reserved names
+    reserved_names = ['CON', 'PRN', 'AUX', 'NUL', 'COM1', 'COM2', 'COM3', 'COM4', 
+                      'COM5', 'COM6', 'COM7', 'COM8', 'COM9', 'LPT1', 'LPT2', 
+                      'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9']
+    if dirname.upper() in reserved_names:
+        dirname = '_' + dirname
+
+    return dirname
+
+def sanitize_path(path):
+    # Split the path into directory names
+    parts = path.split('/')
+
+    # Sanitize each directory name
+    parts = [sanitize_dirname(part) for part in parts]
+
+    # Rejoin the sanitized directory names into a path
+    sanitized_path = '/'.join(parts)
+
+    return sanitized_path
+
+def clean_filename(filename, replacement='_'):
+    # Illegal characters for file names in Unix-based systems
+    invalid_unix = {'/', '\0'}
+
+    # In Windows, some characters might be valid in a path, but not in a file name
+    invalid_windows = {'<', '>', ':', '"', '/', '\\', '|', '?', '*'}
+
+    # Merge invalid character sets
+    invalid = invalid_unix.union(invalid_windows)
+
+    # Create translation table from invalid characters to the replacement character
+    trans_table = str.maketrans({char: replacement for char in invalid})
+
+    # Clean the filename
+    clean_name = filename.translate(trans_table)
+
+    # In addition to the above, the names "." and ".." are not valid names in Windows, so handle those separately
+    if clean_name in {".", ".."}:
+        clean_name = replacement
+
+    return clean_name
+
+def extract_html(byte_list):
+    response_index = -1
+    for idx, line in enumerate(byte_list):
+        if line.startswith(b'HTTP'):
+            if b'200' in line:
+                response_index = idx
+                break
+    if response_index == -1:
+        return ""
+
+    start_index = -1
+    end_index = -1
+    byte_list = byte_list[response_index + 1:]
+
+    for idx, line in enumerate(byte_list):
+        if line.startswith(b'<!DOCTYPE'):
+            start_index = idx
+        if line.startswith(b'</html>'):
+            end_index = idx
+        if start_index != -1 and end_index != -1:
+            break
+    
+    if start_index != -1 and end_index != -1:
+        html_content = b''.join(byte_list[start_index:end_index]).decode('utf-8', errors="ignore")
+    else:
+        html_content = ""
+
+    return html_content
+
+def is_english(soup):
+    try:
+        # Extract all the text from the page
+        text = ' '.join(soup.stripped_strings)
+
+        # Detect the languages
+        detections = detect_langs(text)
+
+        for detection in detections:
+            if detection.lang == 'en' and detection.prob > 0.6:
+                return True
+        return False
+    except:
+        return False
 
 if __name__ == "__main__":
 
-    file_name = "../data/law0.warc.gz"
-    file_size = os.stat(file_name).st_size
+    for i in range(8):  # Go up to but not including 8
+        file_name = f"../data/law{i}.warc.gz"
+        print(f"Now processing: {file_name}")
+        file_size = os.stat(file_name).st_size
 
-    warc_re = re.compile(r'warc/0.9')
-    http_re = re.compile(r'HTTP/1\.(0|1)\s[0-9]{3}\s(\w+)')
-    
-    # HTTP_STATUS_CODES = ['301', '302', '403']
-
-    total_records = 0
-
-    with tqdm(total=file_size, unit_scale=True, unit_divisor=1024, unit="B") as pbar:
-        with gzip.open(file_name, 'r') as warc_file:
-            
-            record = None
-            body = b""
-
-            for line in warc_file:
-                try:
-                    if warc_re.search(str(line)):
-                        if record is not None:
-                            total_records += 1
-
-                            # print(str(body.decode('utf-8', errors="ignore")))
-                            record = None
-                            body = b""
-                        else: 
-                            record = WARCRecord(*(str(line).split()))
-
-                            # path = urlparse(record.subject_uri).path
-                            # if path.rsplit('.', 1)['/', '/index', '/home', '/homepage']:
-                            #     print(path)
-                            #     record = None
-                    elif record is not None:
-                        body += line
-                        # Throw away HTTP with Status Code 3XX or 4XX
-                        if http_re.search(str(line)):
-                            http_status = str(line)[11:14].rstrip()
-                            if http_status != '200':
-                                record = None
-                                body = b""
-                except (Exception) as error:
-                    logging.error("An error occurred while processing..")
-                    logging.error(error)
+        with tqdm(total=file_size, unit_scale=True, unit_divisor=1024, unit="B") as pbar:
+            with gzip.open(file_name, 'r') as warc_file:
                 
-                pbar.update(len(line))
-            
-            # Do something with last record
-        
-print(f'Total Number of Records in the file is {total_records}')
+                record = None
+                byte_list = []
+                
+                for line in warc_file:
+                    pbar.update(len(line))
+                    try:
+                        if line.startswith(b'warc/0.9'):
+                            if record is not None:
+                                try:
+                                    url = urlparse(record.subject_uri)
+                                    domain = url.netloc
+                                    path = url.path
+
+                                    if path == '/':
+                                        path = '/index.html'
+                                    path = os.path.normpath(path)
+                                    path, filename = os.path.split(path)
+                                    path = sanitize_path(path)
+
+                                    filename = clean_filename(filename)
+                                    basedir = os.path.join('html', domain, path.strip("\\"))
+
+                                    if not os.path.exists(os.path.join(basedir, filename)):
+                                        if (path == ''):
+                                            html = extract_html(byte_list)
+                                            soup = BeautifulSoup(html, features='html.parser')
+
+                                            if is_english(soup):
+                                                if not os.path.isdir(basedir):
+                                                    os.makedirs(basedir, exist_ok=True)
+
+                                                with open(os.path.join(basedir, filename), 'w', encoding='utf-8') as f:            
+                                                    f.write(soup.prettify())
+                                                    logging.info('Finished parsing %s' % record.subject_uri)
+
+                                except Exception as e:
+                                    raise Exception(e)
+                                finally:
+                                    record = None
+                                    byte_list = []
+                            else: 
+                                record = WARCRecord(*(str(line).split()))
+                        elif record is not None:
+                            byte_list.append(line)
+                    except Exception as e:
+                        logging.error(e)
+                
+                # Do something with last record
